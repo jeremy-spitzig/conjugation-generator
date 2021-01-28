70236b164147bab0148f55f494cb43ea74e73ba6
jeremy-spitzig <jpspitzig@gmail.com>
2021-01-28 17:05:54 -0300

    Split output into max 2000 line files

--- main.go
+++ main.go
@@ -13,22 +13,29 @@ import (
 func main() {
 	ifn := os.Getenv("VERB_INPUT_FILE")
 	ofn := os.Getenv("SENTENCE_OUTPUT_FILE")
+	ofe := os.Getenv("SENTENCE_OUTPUT_EXT")
 	if ifn == "" {
 		ifn = "./input.json"
 	}
 	if ofn == "" {
-		ofn = "./output.csv"
+		ofn = "./output"
+	}
+	if ofe == "" {
+		ofe = ".csv"
 	}
 	verbs, err := verbs.ReadVerbs(ifn)
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	of, err := os.OpenFile(ofn, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0644)
+	index := 1
+
+	of, err := getFile(ofn, ofe, index)
 	if err != nil {
 		log.Panicln(err)
 	}
-	defer of.Close()
+
+	lines := 0
 
 	for _, v := range verbs {
 		gs, err := sentences.GenerateSentences(v)
@@ -36,7 +43,25 @@ func main() {
 			log.Panicln(err)
 		}
 		for _, s := range gs {
+			if lines >= 2000 {
+				lines = 0
+				index++
+				of, err = getFile(ofn, ofe, index)
+				if err != nil {
+					log.Panicln(err)
+				}
+			}
 			fmt.Fprint(of, s.English, ";", s.Português, "\n")
+			lines++
 		}
 	}
+	of.Close()
+}
+
+func getFile(ofn, ofe string, index int) (*os.File, error) {
+	of, err := os.OpenFile(fmt.Sprintf("%s-%d%s", ofn, index, ofe), os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return of, nil
 }
